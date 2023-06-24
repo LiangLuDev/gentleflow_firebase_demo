830e3200b19a5bcc2534f3d1802c38fd7db09faa
leo.lu <liangludev@gmail.com>
2023-06-25 01:17:03 +0800

    macos sign in success

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:desktop_webview_auth/desktop_webview_auth.dart';
+import 'package:desktop_webview_auth/google.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:gentleflow_firebase_demo/firebase_options.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  FirebaseApp firebaseApp = await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseAuth.instanceFor(app: firebaseApp);
   runApp(const MyApp());
 }
 
@@ -13,21 +26,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -39,15 +37,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,71 +44,127 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Future<User?> signInWithGoogle() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    try {
+      UserCredential? userCredential;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+      if (kIsWeb) {
+        var googleProvider = GoogleAuthProvider();
+        userCredential = await auth.signInWithPopup(googleProvider);
+      }else if (Platform.isMacOS || Platform.isWindows|| Platform.isLinux) {
+        final result = await DesktopWebviewAuth.signIn(GoogleSignInArgs(
+          clientId:
+          DefaultFirebaseOptions.desktop.iosClientId ?? '',
+          redirectUri: 'https://gentleflow-firebase.firebaseapp.com/__/auth/handler',
+        ));
+        print('result ${result.toString()}');
+        if (result != null) {
+          final credential = GoogleAuthProvider.credential(
+            idToken: result.idToken,
+            accessToken: result.accessToken,
+          );
+          userCredential = await auth.signInWithCredential(credential);
+        }
+      }else {
+        final GoogleSignInAccount googleUser = (await GoogleSignIn().signIn())!;
+        final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+        final googleAuthCredential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+        userCredential = await auth.signInWithCredential(googleAuthCredential);
+      }
+
+      final user = userCredential?.user;
+      return user;
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            StreamBuilder(
+                stream: FirebaseAuth.instance.authStateChanges(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    print('snapshot ${snapshot.data}');
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('${snapshot.data?.toString()} --- ${snapshot.data?.email}--- ${snapshot.data?.displayName}'),
+                    );
+                  } else {
+                    print('snapshot ${snapshot.error}');
+                    return Text('无数据');
+                  }
+                }),
+            TextButton(
+                onPressed: () {
+                  save();
+                },
+                child: Text('Set')),
+            TextButton(
+                onPressed: () {
+                  get();
+                },
+                child: Text('Get')),TextButton(
+                onPressed: () {
+                  update();
+                },
+                child: Text('Update')),TextButton(
+                onPressed: () {
+                  logout();
+                },
+                child: Text('Signout')),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: signInWithGoogle,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  save() {
+    User? user = FirebaseAuth.instance.currentUser;
+    print('user ${user.toString()}');
+    final docData = {
+      "displayName": user?.displayName,
+      "uid": user?.uid,
+      "email": user?.email,
+      "isVip": false,
+    };
+    FirebaseFirestore.instance.collection('member').doc(user?.uid).set(docData);
+  }
+
+  get() async{
+    User? user = FirebaseAuth.instance.currentUser;
+    var future = await FirebaseFirestore.instance.collection('member').doc(user?.uid).get();
+    print('future ${future.data()}');
+  }
+
+  bool isUpdate = false;
+  update() async{
+    isUpdate = !isUpdate;
+    User? user = FirebaseAuth.instance.currentUser;
+    await FirebaseFirestore.instance.collection('member').doc(user?.uid).update({
+      'isVip': isUpdate
+    });
+  }
+
+  logout() async{
+    FirebaseAuth.instance.signOut();
   }
 }
